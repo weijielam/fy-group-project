fe1d2fc226854de5d96407f702a7dd37d5dd3e9f
Wei <lamw@tcd.ie>
2018-02-25 15:28:42 +0000

    added install.sh file

--- Envs/my-venv/bin/activate.fish
+++ Envs/my-venv/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/jmulcah/Envs/my-venv"
+set -gx VIRTUAL_ENV "/home/lamw/fygroupproject/Envs/my-venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
