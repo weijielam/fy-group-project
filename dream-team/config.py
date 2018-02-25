dadbd647ca0590c4bdb5b978144c9e18a9228a35
teefyl <teefyl@tcd.ie>
2018-02-25 22:13:14 +0000

    Doing nothing

--- dream-team/config.py
+++ dream-team/config.py
@@ -5,14 +5,6 @@ class Config(object):
     Common configurations
     """
 
-
-    app.config['MAIL_SERVER']='smtp.gmail.com'
-    app.config['MAIL_PORT'] = 465
-    app.config['MAIL_USERNAME'] = 'fygptest@gmail.com'
-    app.config['MAIL_PASSWORD'] = 'admin2018'
-    app.config['MAIL_USE_TLS'] = False
-    app.config['MAIL_USE_SSL'] = True
-
     # Put any configurations here that are common across all environments
 
 class DevelopmentConfig(Config):
