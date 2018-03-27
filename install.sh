e3956f67c7976c42e032b8372692eb607feb8f55
Wei <lamw@tcd.ie>
2018-03-27 16:41:13 +0100

    edited bash scripts, hope they fucking work

--- install.sh
+++ install.sh
@@ -15,6 +15,12 @@ pip install virtualenv
 # CREATE DATABASE dreamteam_db;
 # GRANT ALL PRIVILEGES ON dreamteam_db . * TO 'dt_admin'@'localhost';
 
+mysql -u root -p << EOF
+CREATE USER 'dt_admin'@'localhost' IDENTIFIED BY 'dt2016';
+CREATE DATABASE dreamteam_db;
+GRANT ALL PRIVILEGES ON dreamteam_db . * TO 'dt_admin'@'localhost';
+EOF
+
 
 source Envs/my-venv/bin/activate
 pip install -r requirements.txt
