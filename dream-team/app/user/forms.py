0e3e422948815f029c1d35bdfe78ca2945f5892b
patrickberry1 <patrickberry1@live.ie>
2018-03-27 16:26:06 +0100

    fixed issue with 0 input values

--- dream-team/app/user/forms.py
+++ dream-team/app/user/forms.py
@@ -2,13 +2,13 @@
 
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, SelectField, IntegerField
-from wtforms.validators import DataRequired, NumberRange
+from wtforms.validators import DataRequired, NumberRange, InputRequired
 
-list_choices = [('Don', 'Donation'), ('Pl', 'Pledge'), ('Ti', 'Ticket')]
+list_choices = [('Don', 'Donation'), ('Pl', 'Pledge')]
 
 class paymentForm(FlaskForm):
-	dol_amount = IntegerField('$', validators=[DataRequired()])
-	cent_amount = IntegerField('.', validators=[DataRequired(), NumberRange(min=0, max=99, message="please enter a number between 0 and 99")])
+	dol_amount = IntegerField('$', validators=[InputRequired()])
+	cent_amount = IntegerField('.', validators=[InputRequired(), NumberRange(min=0, max=99, message="please enter a number between 0 and 99")])
 	pay_type = SelectField('Type', choices = list_choices, validators=[DataRequired()])
 	event = SelectField('Event', coerce=int)
 	submit = SubmitField('Submit')
