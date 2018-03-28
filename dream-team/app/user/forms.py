c89bb2aabe12bea549077ee8c3b43795e8bccec1
James Mulcahy <jmulcah@tcd.ie>
2018-03-28 22:47:38 +0100

    changed euro and cent in form for payment, sorry laura

--- dream-team/app/user/forms.py
+++ dream-team/app/user/forms.py
@@ -7,8 +7,8 @@ from wtforms.validators import DataRequired, NumberRange, InputRequired
 list_choices = [('Donation', 'Donation'), ('Pledge', 'Pledge')]
 
 class paymentForm(FlaskForm):
-	dol_amount = IntegerField('$', validators=[InputRequired()])
-	cent_amount = IntegerField('.', validators=[InputRequired(), NumberRange(min=0, max=99, message="please enter a number between 0 and 99")])
+	dol_amount = IntegerField('Euro', validators=[InputRequired()])
+	cent_amount = IntegerField('Cents', validators=[InputRequired(), NumberRange(min=0, max=99, message="please enter a number between 0 and 99")])
 	pay_type = SelectField('Type', choices = list_choices, validators=[DataRequired()])
 	purpose = SelectField('Purpose', coerce=int)
 	submit = SubmitField('Submit')
